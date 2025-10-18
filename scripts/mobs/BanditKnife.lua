4119897a483373a9a0ffd4754c66e32331c30700
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-10-18 23:34:35 +0300

    v48 - BanditKnife fix

--- scripts/mobs/BanditKnife.lua
+++ scripts/mobs/BanditKnife.lua
@@ -7,38 +7,38 @@
 
 local RPD = require "scripts/lib/epicClasses"
 
-local mob = require"scripts/lib/mob"
+local mob = require "scripts/lib/mob"
 
-local function teleport(self)
-local x = level:cellX(self:getPos())
-local y = level:cellY(self:getPos())
-for i = x - 3, x + 3 do
-for j = y - 3, y + 3 do
-local pos = RPD.Dungeon.level:cell(i,j)
-if math.random(1,5) == 1 then
-self:getSprite():emitter():burst(RPD.Sfx.Speck:factory(RPD.Sfx.Speck.WOOL ), 4)
-local dst = RPD.Ballistica:cast(self:getPos(),pos,true,true,false)
-self:move(dst)
-self:getSprite():move(self:getPos(),dst)
-end
-end
-end
-end
-
-return mob.init({ 
-
-zapProc = function(self, enemy, dmg)
-local level = RPD.Dungeon.level
-if level:distance(self:getPos(),enemy:getPos()) < 3 then
-teleport(self)
-end
-return dmg
-end,
-attackProc = function(self, enemy, dmg)
-teleport(self)
-return dmg
+local function teleport(chr, level)
+    local opos = chr:getPos()
+    local x = level:cellX(opos)
+    local y = level:cellY(opos)
+    for i = x - 3, x + 3 do
+        for j = y - 3, y + 3 do
+            local pos = RPD.Dungeon.level:cell(i, j)
+            if math.random(1, 5) == 1 then
+                chr:getSprite():emitter():burst(RPD.Sfx.Speck:factory(RPD.Sfx.Speck.WOOL), 4)
+                local dst = RPD.Ballistica:cast(opos, pos, true, true, false)
+                chr:move(dst)
+                chr:getSprite():move(opos, dst)
+                return
+            end
+        end
+    end
 end
 
+return mob.init({
+    zapProc = function(self, enemy, dmg)
+        local level = RPD.Dungeon.level
+        if level:distance(self:getPos(), enemy:getPos()) < 3 then
+            teleport(self, level)
+        end
+        return dmg
+    end,
+    attackProc = function(self, enemy, dmg)
+        teleport(self)
+        return dmg
+    end
 })
 
 
